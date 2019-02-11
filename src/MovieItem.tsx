3b659e2b110c280c8fc57b945b25c43a1a13a73a
will-lp <will_lp@ymail.com>
2019-02-11 15:40:22 -0200

    Ficha dos filmes estilizada.

--- src/MovieItem.tsx
+++ src/MovieItem.tsx
@@ -3,6 +3,8 @@ import React, { Component  } from 'react';
 
 import FavoriteCheck from './FavoriteCheck';
 
+import styled from 'styled-components';
+
 export default class MovieItem extends Component<any> {
 	
 	movie;
@@ -16,15 +18,58 @@ export default class MovieItem extends Component<any> {
 	
 	render() {
 		return (
-			<div>
-				<img src={this.movie.Poster}></img>
-				<p>{this.movie.Title}</p>
-				<FavoriteCheck 
-					movie={this.movie}
-					rootStore={this.props.rootStore} />
-			</div>
+			<MovieInfo>
+				<Poster>
+					<img src={this.movie.Poster}></img>
+				</Poster>
+				<MovieInformation>
+					<li><b>Título:</b> {this.movie.Title}</li>
+					<li><b>Lançamento:</b> {this.movie.Released}</li>
+					<li><b>Duração:</b> {this.movie.Runtime}</li>
+					<li><b>Gênero:</b> {this.movie.Genre}</li>
+					<li><b>Diretor:</b> {this.movie.Director}</li>
+					<li><b>Escritor:</b> {this.movie.Writer}</li>
+					<li><b>Atores:</b> {this.movie.Actors}</li>
+					<li><b>Sinopse:</b> {this.movie.Plot}</li>
+					<li><b>Avaliações:</b>
+						<ul>
+						{ this.movie.Ratings.map(rating => 
+							<li>{rating.Source}: {rating.Value}</li>)
+						}
+						</ul>
+					</li>
+				</MovieInformation>
+				<FavoriteCheckCol>
+						<div>
+						<FavoriteCheck 
+							movie={this.movie}
+							rootStore={this.props.rootStore} />
+						</div>
+				</FavoriteCheckCol>
+			</MovieInfo>
 		);
 	}
 
-
 }
+
+
+const MovieInfo = styled.div.attrs({ className: '' })`
+	display: flex; 
+	border: 1px solid #999;
+	margin-bottom: 1em;
+`;
+const Poster = styled.div.attrs({ className: '' })`
+	display: inline-block; 
+`;
+const MovieInformation = styled.ul.attrs({ className: '' })`
+	display: inline-block;
+	padding-top: 1em;
+	flex: 1;
+`;
+const FavoriteCheckCol = styled.div.attrs({ className: '' })`
+	padding: 1em;
+	float: right;
+	text-align: right;
+	width: 50px;
+`;
+
